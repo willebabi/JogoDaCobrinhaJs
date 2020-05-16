9ec6e06b62c3f4aee706ca43ca1485eff9429500
Willian Wagner <will.wag@gmail.com>
2020-05-15 21:49:03 -0300

    Criando a cobrinha

--- script.js
+++ script.js
@@ -1,10 +1,21 @@
 let vplano = document.getElementById("plano");
 let vctx   = vplano.getContext("2d");
 let vbox = 32;
-
+let vcobra = [];
+vcobra[0] = {
+    x: 8 * vbox,
+    y: 8 * vbox
+}
 function criarBG () {
     vctx.fillStyle = "lightgreen";
     vctx.fillRect(0,0,16 * vbox,16 * vbox);
+}
+
+function criaCobra () {
+    for (i=0; i < vcobra.length; i++) {
+        vctx.fillStyle = "green";
+        vctx.fillRect(vcobra[i].x,vcobra[i].y,vbox,vbox);
+    }
 }
 
 criarBG();
